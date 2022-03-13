9432b5d5c1864316d5b220b92933aa7822823aff
niki4smirn <kitos.smirnov@gmail.com>
2022-03-13 11:04:11 +0300

    Add QListWidget and QSpinBox, that controls QListWidget items count

--- main_window.cpp
+++ main_window.cpp
@@ -1,1 +1,28 @@
 #include "main_window.h"
+
+MainWindow::MainWindow() :
+    QMainWindow(nullptr),
+    widget_(new QWidget(this)),
+    layout_(new QGridLayout()) {
+  resize(minimal_size_);
+  setMinimumSize(minimal_size_);
+
+  count_ = new QSpinBox(widget_);
+  questions_list_ = new QListWidget(widget_);
+  layout_->addWidget(count_, 0, 0);
+  layout_->addWidget(questions_list_, 1, 1);
+  widget_->setLayout(layout_);
+  setCentralWidget(widget_);
+  ConnectWidgets();
+}
+
+void MainWindow::ConnectWidgets() {
+  connect(count_, QOverload<int>::of(&QSpinBox::valueChanged), [&](int i) {
+    while (i < questions_list_->count()) {
+      questions_list_->takeItem(questions_list_->count() - 1);
+    }
+    while (i > questions_list_->count()) {
+      questions_list_->addItem(new QListWidgetItem(QString::number(i), questions_list_));
+    }
+  });
+}
--- main_window.h
+++ main_window.h
@@ -1,9 +1,22 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QSpinBox>
+#include <QListWidget>
+#include <QGridLayout>
 
 class MainWindow : public QMainWindow {
  Q_OBJECT
  public:
-  MainWindow() = default;
+  MainWindow();
+
+ private:
+  void ConnectWidgets();
+
+  QWidget* widget_;
+  QGridLayout* layout_;
+  QSpinBox* count_;
+  QListWidget* questions_list_;
+
+  QSize minimal_size_{300, 300};
 };
